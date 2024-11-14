6e9bcd9e29a0533598378604813600266482b72f
Altorook <nathanberry403@gmail.com>
2024-11-13 17:36:24 -0700

    Prefab materials made

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -23,15 +23,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get input axis
         playerXVelocity = Input.GetAxis("Horizontal");
         playerZVelocity = Input.GetAxis("Vertical");
         mouseUpRotation += Input.GetAxis("Mouse Y") * _mouseSens;
         mouseRightRotation = Input.GetAxis("Mouse X") * _mouseSens;
+        //rotate camera vertically
         _cameraTransform.localEulerAngles = new Vector3(Mathf.Clamp(-mouseUpRotation, -camVerticalClamp, camVerticalClamp),0, 0);
     }
     private void FixedUpdate()
     {
+        //rotate player
         _playerTransform.Rotate(new Vector3(0,mouseRightRotation, 0));
+        //set player velocity
         _playerRB.velocity = (playerXVelocity * transform.right  + playerZVelocity * transform.forward ).normalized * _speed;
     }
 }
